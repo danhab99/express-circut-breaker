7727364c737a2ca80f851c368cb616abc4f92590
Dan Habot <dan.habot@gmail.com>
2020-02-25 23:31:40 -0500

    Added testing

---
+++ test.js
@@ -0,0 +1,90 @@
+var assert = require('assert')
+const request = require('supertest')
+var breaker = require('./index')
+
+describe('express-circut-breaker', function() {
+  var app = require('express')()
+  var forceReset = false
+
+  app.get('/unprotected', (req, res) => res.sendStatus(200))
+
+  app.get('/reset', (req, res) => {
+    forceReset = true
+    res.sendStatus(200)
+  })
+
+  var CB = breaker({
+    catchError: e => 'trip',
+    handleBlockedRequest: (req, res) => {
+      if (forceReset) {
+        req.resetBreaker()
+        res.sendStatus(200)
+      }
+      else {
+        res.sendStatus(500)
+      }
+    }
+  })
+
+  app.get('/protected', CB, (req, res) => {
+    res.sendStatus(200)
+  })
+
+  app.get('/protected/crash', CB, (req, res) => {
+    throw 1
+  })
+
+  it('can hit an endpoint', function() {
+    request(app)
+      .get('/unprotected')
+      .expect(200)
+      .end((err, res) => {
+        assert.ok(res)
+      })
+  })
+
+  it('can hit a protected endpoint', function() {
+    request(app)
+      .get('/protected')
+      .expect(200)
+      .end((err, res) => {
+        assert.ok(res)
+      })
+  })
+
+  it('can trip the circut breaker', function() {
+    request(app)
+      .get('/protected/crash')
+      .expect(500)
+      .end((err, res) => {
+        assert.ok(res)
+      })
+  })
+
+  it('will be stopped by a tripped circut breaker', function() {
+    request(app)
+      .get('/protected')
+      .expect(500)
+      .end((err, res) => {
+        assert.ok(res)
+      })
+  })
+
+  it('will reset the circut breaker', function() {
+    request(app)
+      .get('/reset')
+      .expect(200)
+      .end((err, res) => {
+        assert.ok(res)
+      })
+  })
+
+  it('can hit a protected endpoint again', function() {
+    request(app)
+      .get('/protected')
+      .expect(200)
+      .end((err, res) => {
+        assert.ok(res)
+      })
+  })
+})
