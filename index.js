faf8a8e1dbb91a9ae8f828e28bc096c12c337872
Dan Habot <dan.habot@gmail.com>
2020-02-25 22:58:29 -0500

    Initial model created

---
+++ index.js
@@ -0,0 +1,56 @@
+
+module.exports = (opts) => {
+  var tripped = false
+  var handle = true
+
+  let keys = Object.keys(opts)
+  let has = x => keys.includes(x)
+
+  if (!has('catchError')) {
+    throw new Error('Requires catchError')
+  }
+
+  if (has('handleLater') && !opts.handleLater) {
+    handle = false
+  }
+
+  if (has('handleLater') && opts.handleLater && !has('handleBlockedRequest')) {
+    throw new Error('Requires handleBlockedRequest')
+  }
+
+  return (req, res, next) => {
+    try {
+      if (!tripped || opts.handleLater) {
+        if (opts.handleLater) {
+          req.breakerTripped = true
+        }
+        else {
+          req.breakerTripped = false
+        }
+        next()
+      }
+      else {
+        if (handle) {
+          opts.handleBlockedRequest(req, res, next)
+        }
+      }
+    }
+    catch (e) {
+      Promise.resolve(opts.catchError(e)).then(act => {
+        if (typeof(act) != 'string') {
+          throw new Error('catchError must resolve/return a string')
+        }
+  
+        if (act == 'trip') {
+          tripped = true
+          return
+        }
+
+        if (act == 'reset') {
+          tripped = false
+          return
+        }
+      })
+    }
+  }
+}
